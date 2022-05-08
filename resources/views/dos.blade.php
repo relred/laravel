3daef22b99658c5a103b9713458ae9cfbf47efa2
Omar Izabal <dev@relred.com>
2022-05-07 22:48:31 -0700

    Commit de repositorio

---
+++ resources/views/dos.blade.php
@@ -0,0 +1,23 @@
+@extends('welcome')
+
+@section('title')
+Componente dos
+@endsection
+
+
+
+@section('content')
+
+
+    <h2>
+        Objeto DOS
+    </h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit neque sapiente dolorum sit nostrum quod fugit maxime fugiat hic suscipit deleniti natus, modi reprehenderit eos excepturi! Dolore, nemo obcaecati.
+    </p>
+
+    <div class="container alert alert-danger">
+        Alerta
+    </div>
+
+@endsection
---
+++ resources/views/uno.blade.php
@@ -0,0 +1,25 @@
+
+@extends('welcome')
+
+@section('title')
+titulo UNO
+@endsection
+
+@section('content')
+
+<div>
+    <div>
+        <ul>
+            <li>uno</li>
+            <li>nonoi</li>
+            <li>onoinoin</li>
+            <li>oinoin</li>
+            <li>oinoinoi</li>
+            <li>noin</li>
+            <li>on</li>
+            <li>oinoi</li>
+        </ul>
+    </div>
+</div>
+
+@endsection
